5d73bc7ec7bdfb0ed1178d6b31a71bd233aed8d7
Mike <mike@excelsior-solutions.de>
2020-05-19 15:43:33 +0200

    Initial Commit

---
+++ HK/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HK
+//
+//  Created by Mike on 19.05.20.
+//  Copyright © 2020 Mike. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
