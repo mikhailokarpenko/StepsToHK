a96bff656bb71e74b6ce176d36a1fa7724130002
Mike <mike@excelsior-solutions.de>
2020-05-19 17:30:14 +0200

    added countLabel and totalLabel

--- HK/ViewController.swift
+++ HK/ViewController.swift
@@ -11,33 +11,23 @@ import HealthKit
 
 class ViewController: UIViewController {
     
-    var healthStore: HKHealthStore?
-    var typesToShare : Set<HKSampleType> {
-        let stepType = HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.stepCount)!
-      return [stepType]
-    }
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-    }
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var totalCountLabel: UILabel!
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
-        ViewController.authorizeHealthKit { (success, error) in
+        authorizeHealthKit { (success, error) in
             print("authorized: \(success)")
         }
     }
 
     @IBAction func buttonPressed(_ sender: Any) {
         let value = Int.random(in: 500...1000)
-        ViewController.saveSteps(stepsCountValue: value, date: Date()) { (error) in
-            print("is saved: \(error)")
-        }
+        saveSteps(stepsCountValue: value, date: Date())
     }
     
-    public class func authorizeHealthKit(completion: @escaping (Bool, Error?) -> Swift.Void) {
+    func authorizeHealthKit(completion: @escaping (Bool, Error?) -> Swift.Void) {
            guard HKHealthStore.isHealthDataAvailable() else {
             print("error")
                return
@@ -57,13 +47,17 @@ class ViewController: UIViewController {
            HKHealthStore().requestAuthorization(toShare: healthKitTypesToWrite,
                                                 read: healthKitTypesToRead) { (success, error) in
                                                    completion(success, error)
+                                                    self.getTodaysSteps { (count) in
+                                                        DispatchQueue.main.async {
+                                                            self.totalCountLabel.text = "Total count: \(count)"
+                                                        }
+                                                    }
            }
            
        }
     
-    public class func saveSteps(stepsCountValue: Int,
-                             date: Date,
-                             completion: @escaping (Error?) -> Swift.Void) {
+    func saveSteps(stepsCountValue: Int,
+                             date: Date) {
         
         guard let stepCountType = HKQuantityType.quantityType(forIdentifier: .stepCount) else {
             fatalError("Step Count Type is no longer available in HealthKit")
@@ -80,15 +74,40 @@ class ViewController: UIViewController {
         
         HKHealthStore().save(stepsCountSample) { (success, error) in
             
-            if let error = error {
-                completion(error)
-                print("Error Saving Steps Count Sample: \(error.localizedDescription)")
-            } else {
-                completion(nil)
-                print("Successfully saved Steps Count Sample")
+            DispatchQueue.main.async {
+                if let error = error {
+                    self.label.text = "ERROR: \(error)"
+                    self.label.sizeToFit()
+                } else {
+                    self.label.text = "Successfully added \(stepsCountValue)"
+                    self.label.sizeToFit()
+                    self.getTodaysSteps { (count) in
+                        DispatchQueue.main.async {
+                            self.totalCountLabel.text = "Total count: \(count)"
+                        }
+                    }
+                }
             }
         }
         
+    }
+    
+    func getTodaysSteps(completion: @escaping (Double) -> Void) {
+        let stepsQuantityType = HKQuantityType.quantityType(forIdentifier: .stepCount)!
+
+        let now = Date()
+        let startOfDay = Calendar.current.startOfDay(for: now)
+        let predicate = HKQuery.predicateForSamples(withStart: startOfDay, end: now, options: .strictStartDate)
+
+        let query = HKStatisticsQuery(quantityType: stepsQuantityType, quantitySamplePredicate: predicate, options: .cumulativeSum) { _, result, _ in
+            guard let result = result, let sum = result.sumQuantity() else {
+                completion(0.0)
+                return
+            }
+            completion(sum.doubleValue(for: HKUnit.count()))
+        }
+
+        HKHealthStore().execute(query)
     }
     
 }
